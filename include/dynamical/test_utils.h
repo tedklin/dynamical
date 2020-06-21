af815ade9f207cac49ce2203320da51cf7e10280
tedklin <tedklin@gmail.com>
2020-06-21 16:05:13 -0700

    add error msg in custom matrix equality check

--- include/dynamical/test_utils.h
+++ include/dynamical/test_utils.h
@@ -19,6 +19,9 @@ bool check_matrix_equality(Eigen::MatrixXd expected, Eigen::MatrixXd actual,
   for (int col = 0; col != expected.cols(); ++col) {
     for (int row = 0; row != expected.rows(); ++row) {
       if (std::abs(expected(row, col) - actual(row, col)) > tolerance) {
+        std::cout << "(dynamical) matrix equality check error: expected\n"
+                  << expected(row, col) << "\n\nbut got\n"
+                  << actual(row, col) << "\n\n";
         return false;
       }
     }
@@ -38,6 +41,9 @@ bool check_complex_matrix_equality(Eigen::MatrixXcd expected,
   for (int col = 0; col != expected.cols(); ++col) {
     for (int row = 0; row != expected.rows(); ++row) {
       if (std::abs(expected(row, col) - actual(row, col)) > tolerance) {
+        std::cout << "(dynamical) matrix equality check error: expected\n"
+                  << expected(row, col) << "\n\nbut got\n"
+                  << actual(row, col) << "\n\n";
         return false;
       }
     }
