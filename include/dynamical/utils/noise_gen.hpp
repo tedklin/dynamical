f8e917faa3f48a5a17278c6100a981dd175ba198
tedklin <tedklin@gmail.com>
2020-07-26 20:41:52 -0700

    separate interface / impl for noise gen

--- include/dynamical/utils/noise_gen.hpp
+++ include/dynamical/utils/noise_gen.hpp
@@ -15,42 +15,46 @@ namespace dynamical {
 
 class NoiseGenerator {
  public:
-  NoiseGenerator(Eigen::Matrix<double, Eigen::Dynamic, 1> stddev)
-      : dimension_(stddev.rows()) {
-    std::random_device rd;
-    generator_ = std::make_shared<std::mt19937>(rd());
-    for (int i = 0; i != stddev.rows(); ++i) {
-      if (stddev(i, 0) < 0) {
-        throw dynamical_error("noise generation: stddev less than 0!");
-      }
-      if (stddev(i, 0) == 0) {
-        // Create empty shared_ptr as placeholder for an element with no noise.
-        distributions_.emplace_back();
-      } else {
-        distributions_.push_back(
-            std::make_shared<std::normal_distribution<>>(0, stddev(i, 0)));
-      }
-    }
-  }
+  NoiseGenerator(Eigen::Matrix<double, Eigen::Dynamic, 1> stddev);
 
-  const int GetDimension() const { return dimension_; }
+  Eigen::Matrix<double, Eigen::Dynamic, 1> GetNoise();
 
-  Eigen::Matrix<double, Eigen::Dynamic, 1> GetNoise() {
-    Eigen::Matrix<double, Eigen::Dynamic, 1> w(dimension_);
-    for (int i = 0; i != w.rows(); ++i) {
-      if (distributions_[i]) {
-        w(i, 0) = (*distributions_[i])(*generator_);
-      } else {
-        w(i, 0) = 0;
-      }
-    }
-    return w;
-  }
+  int GetDimension() const { return dimension_; }
 
  private:
   const int dimension_;
   std::shared_ptr<std::mt19937> generator_;
   std::vector<std::shared_ptr<std::normal_distribution<>>> distributions_;
 };
+
+NoiseGenerator::NoiseGenerator(Eigen::Matrix<double, Eigen::Dynamic, 1> stddev)
+    : dimension_(stddev.rows()) {
+  std::random_device rd;
+  generator_ = std::make_shared<std::mt19937>(rd());
+  for (int i = 0; i != stddev.rows(); ++i) {
+    if (stddev(i, 0) < 0) {
+      throw dynamical_error("noise generation: stddev less than 0!");
+    }
+    if (stddev(i, 0) == 0) {
+      // Create empty shared_ptr as placeholder for an element with no noise.
+      distributions_.emplace_back();
+    } else {
+      distributions_.push_back(
+          std::make_shared<std::normal_distribution<>>(0, stddev(i, 0)));
+    }
+  }
+}
+
+Eigen::Matrix<double, Eigen::Dynamic, 1> NoiseGenerator::GetNoise() {
+  Eigen::Matrix<double, Eigen::Dynamic, 1> w(dimension_);
+  for (int i = 0; i != w.rows(); ++i) {
+    if (distributions_[i]) {
+      w(i, 0) = (*distributions_[i])(*generator_);
+    } else {
+      w(i, 0) = 0;
+    }
+  }
+  return w;
+}
 
 }  // namespace dynamical
